ca23d01305d32e0351c11b37db81526d51159e37
yujie cao <yujiecao@mobilenet1-3-10-25-138-189.wnet.ucr.edu>
2017-09-16 15:55:44 -0700

    add more files

--- service/main.go
+++ service/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	elastic "gopkg.in/olivere/elastic.v3"
 	"fmt"
 	"net/http"
 	"encoding/json"
 	"log"
 	"strconv"
+	"reflect"
+	"github.com/pborman/uuid"
 )
-const (
-	DISTANCE = "200km"
-)
+
 
 type Location struct {
 	Lat float64 `json:"lat"`
@@ -23,9 +24,52 @@ type Post struct {
 	Location Location `json:"location"`
 }
 
+const (
+	INDEX = "around"
+	TYPE = "post"
+	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://34.213.213.121:9200"
+)
 
 
 func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+                    "mappings":{
+                           "post":{
+                                  "properties":{
+                                         "location":{
+                                                "type":"geo_point"
+                                         }
+                                  }
+                           }
+                    }
+             }
+             `
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
@@ -44,6 +88,30 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	// Create a client
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	id := uuid.New()
+
+	// Save it to index
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -63,17 +131,67 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Search received: %f %f %s", lat, lon, ran)
 
-	// Return a fake post
-	p := &Post{
-		User:"1111",
-		Message:"一生必去的100个地方",
-		Location: Location{
-			Lat:lat,
-			Lon:lon,
-		},
+	//// Return a fake post
+	//p := &Post{
+	//	User:"1111",
+	//	Message:"一生必去的100个地方",
+	//	Location: Location{
+	//		Lat:lat,
+	//		Lon:lon,
+	//	},
+	//}
+	//
+	//js, err := json.Marshal(p)
+	//if err != nil {
+	//	panic(err)
+	//	return
+	//}
+	//
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Write(js)
+
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
 	}
 
-	js, err := json.Marshal(p)
+	// Define geo distance query as specified in
+	// https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	// Some delay may range from seconds to minutes. So if you don't get enough results. Try it later.
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
 	if err != nil {
 		panic(err)
 		return
@@ -81,4 +199,5 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
+
 }
